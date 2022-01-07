943e9757c78085404ddf7d5dd822ed4f11069a84
Richard Hopkins <hopkira@googlemail.com>
2022-01-07 17:35:58 +0000

    Update k9stt.py

--- k9stt.py
+++ k9stt.py
@@ -1,7 +1,28 @@
 import pvporcupine
+from pvrecorder import PvRecorder
 from secrets import * 
+from datetime import datetime
 
-porcupine = pvporcupine.create(
-  access_key = ACCESS_KEY,
-  keyword_paths=['/home/pi/k9localstt/canine_en_raspberry-pi_v2_0_0.ppn']
-)
+try:
+    porcupine = pvporcupine.create(
+    access_key = ACCESS_KEY,
+    keyword_paths=['/home/pi/k9localstt/canine_en_raspberry-pi_v2_0_0.ppn']
+    )
+
+    recorder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
+    recorder.start()
+
+    print(f'Using device: {recorder.selected_device}')
+
+    while True:
+        pcm = recorder.read()
+        result = porcupine.process(pcm)
+        if result >= 0:
+            print('[%s] Detected %s' % (str(datetime.now()), str(result)))
+except KeyboardInterrupt:
+    print('Stopping....')
+finally:
+    if porcupine is not None:
+        porcupine.delete()
+    if recorder is not None:
+        recorder.delete()
