5db327ea13ec2aed27a571523b9cfcd74ce20ad2
Richard Hopkins <hopkira@googlemail.com>
2022-01-08 21:41:43 +0000

    Create back_lights.py

---
+++ back_lights.py
@@ -0,0 +1,26 @@
+import board
+import busio
+import adafruit_pca9685
+i2c = busio.I2C(board.SCL, board.SDA)
+pca = adafruit_pca9685.PCA9685(i2c)
+pca.frequency = 60
+
+class Eyes():
+    def __init__(self):
+        self.level = 0
+        self.set_level(self.level)
+
+    def on(self):
+        self.level = 1
+        self.state()
+
+    def off(self):
+        self.level = 0
+        self.state()
+
+    def state(self):
+        value = int(self.level * 65535)
+        pca.channels[1].duty_cycle = value
+
+    def get_level(self):
+        return self.level
