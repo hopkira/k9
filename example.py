5b521ee48c148f1dc13aba2f16bea3e586aa2468
Richard Hopkins <hopkira@googlemail.com>
2022-02-25 20:39:18 +0000

    Preview window

--- example.py
+++ example.py
@@ -11,9 +11,9 @@ pipeline = depthai.Pipeline()
 CONF = 0.7
 
 # First, we want the Color camera as the output
-#cam_rgb = pipeline.createColorCamera()
-#cam_rgb.setPreviewSize(300, 300)  # 300x300 will be the preview frame size, available as 'preview' output of the node
-#cam_rgb.setInterleaved(False)
+cam_rgb = pipeline.createColorCamera()
+cam_rgb.setPreviewSize(300, 300)  # 300x300 will be the preview frame size, available as 'preview' output of the node
+cam_rgb.setInterleaved(False)
 
 # Next, we want a neural network that will produce the detections
 detection_nn = pipeline.createMobileNetDetectionNetwork()
@@ -23,14 +23,14 @@ detection_nn.setBlobPath(blobconverter.from_zoo(name='mobilenet-ssd', shaves=8))
 # Next, we filter out the detections that are below a confidence threshold. Confidence can be anywhere between <0..1>
 detection_nn.setConfidenceThreshold(CONF)
 # Next, we link the camera 'preview' output to the neural network detection input, so that it can produce detections
-# cam_rgb.preview.link(detection_nn.input)
+cam_rgb.preview.link(detection_nn.input)
 
 # XLinkOut is a "way out" from the device. Any data you want to transfer to host need to be send via XLink
-#xout_rgb = pipeline.createXLinkOut()
+xout_rgb = pipeline.createXLinkOut()
 # For the rgb camera output, we want the XLink stream to be named "rgb"
-#xout_rgb.setStreamName("rgb")
+xout_rgb.setStreamName("rgb")
 # Linking camera preview to XLink input, so that the frames will be sent to host
-#cam_rgb.preview.link(xout_rgb.input)
+cam_rgb.preview.link(xout_rgb.input)
 
 # The same XLinkOut mechanism will be used to receive nn results
 xout_nn = pipeline.createXLinkOut()
@@ -43,7 +43,7 @@ with depthai.Device(pipeline) as device:
     # From this point, the Device will be in "running" mode and will start sending data via XLink
 
     # To consume the device results, we get two output queues from the device, with stream names we assigned earlier
-    # q_rgb = device.getOutputQueue("rgb")
+    q_rgb = device.getOutputQueue("rgb")
     q_nn = device.getOutputQueue("nn")
 
     # Here, some of the default values are defined. Frame will be an image from "rgb" stream, detections will contain nn results
@@ -61,12 +61,12 @@ with depthai.Device(pipeline) as device:
     # Main host-side application loop
     while True:
         # we try to fetch the data from nn/rgb queues. tryGet will return either the data packet or None if there isn't any
-        # in_rgb = q_rgb.tryGet()
+        in_rgb = q_rgb.tryGet()
         in_nn = q_nn.tryGet()
 
-        #if in_rgb is not None:
+        if in_rgb is not None:
             # If the packet from RGB camera is present, we're retrieving the frame in OpenCV format using getCvFrame
-            #frame = in_rgb.getCvFrame()
+            frame = in_rgb.getCvFrame()
 
         if in_nn is not None:
             # when data from nn is received, we take the detections array that contains mobilenet-ssd results
@@ -76,12 +76,12 @@ with depthai.Device(pipeline) as device:
             for detection in detections:
                 # for each bounding box, we first normalize it to match the frame size
                 print(detection.label, detection.conf, detection.depth_z, detection.depth_x)
-                #bbox = frameNorm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+                bbox = frameNorm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
                 # and then draw a rectangle on the frame to show the actual result
-                #cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
+                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
             # After all the drawing is finished, we show the frame on the screen
-            #cv2.imshow("preview", frame)
+            cv2.imshow("preview", frame)
 
         # at any time, you can press "q" and exit the main loop, therefore exiting the program itself
-        #if cv2.waitKey(1) == ord('q'):
-            #break
+        if cv2.waitKey(1) == ord('q'):
+            break
