4f9a6f977774c9876b43ea050c65d16fbddd08c7
Richard Hopkins <hopkira@googlemail.com>
2022-08-08 17:26:02 +0100

    Simplified point cloud for forward collisions

--- k9_oak_pipe.py
+++ k9_oak_pipe.py
@@ -194,11 +194,76 @@ class Point_Cloud():
         point_cloud = np.nanmin(totals, axis = 0)
         # inject the resulting 40 sensor points into the
         # short term memory of the robot
-        #for index,point in enumerate(point_cloud):
-        #    mem.storeSensorReading("oak",float(point),float(self.angles_array[index]))
-        point_cloud = point_cloud[16:24]
-        min_dist = np.amin(point_cloud)
+        for index,point in enumerate(point_cloud):
+            mem.storeSensorReading("oak",float(point),float(self.angles_array[index]))
+        # point_cloud = point_cloud[16:24]
+        # min_dist = np.amin(point_cloud)
+        # mem.storeState("forward", min_dist)
+
+class Forward_Collision():
+    '''
+    Creates a focussed point cloud that determines any obstacles
+    directly in front of the robot
+    '''
+
+    def __init__(self):
+        # Point cloud loop constants
+        self.x_bins = pd.interval_range(start = -350, end = 350, periods = 7)
+        self.y_bins = pd.interval_range(start = 0, end = 1600, periods = 1)
+        self.fx = 1.4 # values found by measuring known sized objects at known distances
+        self.fy = 3.3
+        self.pc_width = 160
+        self.cx = self.pc_width / 2
+        self.pc_height = 100
+        self.cy = self.pc_height / 2
+        self.pc_max_range  = 10000.0
+        self.pc_min_range  = 200.0
+        self.column, self.row = np.meshgrid(np.arange(self.pc_width), np.arange(self.pc_height), sparse=True)
+
+    def record_point_cloud(self,depth_image):
+        '''
+        Distills the point cloud down to a single value that is the distance to the
+        nearest obstacle that is directly in front of the robot
+        '''
+
+        # Ignore points too close or too far away
+        valid = (depth_image >= self.pc_min_range) & (depth_image <= self.pc_max_range)
+        # Calculate the point cloud using simple extrapolation from depth
+        z = np.where(valid, depth_image, 0.0)
+        x = np.where(valid, (z * (self.column - self.cx) /self.cx / self.fx) + 120.0 , self.pc_max_range)
+        y = np.where(valid, 268 - (z * (self.row - self.cy) / self.cy / self.fy) , self.pc_max_range) # measured height is 268mm
+        z2 = z.flatten()
+        x2 = x.flatten()
+        y2 = y.flatten()
+        cloud = np.column_stack((x2,y2,z2))
+        # Remove points that are projected to fall outside the field of view
+        # points below floor level, above 1.6m or those more than 2m to the
+        # sides of the robot are ignored
+        in_scope = (cloud[:,1] < 1600) & (cloud[:,1] > 0) & (cloud[:,0] < 350) & (cloud[:,0] > -350)
+        in_scope = np.repeat(in_scope, 3)
+        in_scope = in_scope.reshape(-1, 3)
+        scope = np.where(in_scope, cloud, np.nan)
+        scope = scope[~np.isnan(scope).any(axis=1)]
+        # place the points into a set of 10cm square bins
+        x_index = pd.cut(scope[:,0], self.x_bins)
+        y_index = pd.cut(scope[:,1], self.y_bins)
+        binned_depths = pd.Series(scope[:,2])
+        # simplify each bin to a single median value
+        totals = binned_depths.groupby([y_index, x_index]).median()
+        # shape the simplified bins into a 2D array
+        totals = totals.values.reshape(1,7)
+        # for each column in the array, find out the closest
+        # bin; as the robot cannot duck or jump, the
+        # y values are irrelevant
+        point_cloud = np.nanmin(totals, axis = 0)
+        min_dist = np.nanmin(point_cloud)
+        # inject the resulting 40 sensor points into the
+        # short term memory of the robot
         mem.storeState("forward", min_dist)
+        # point_cloud = point_cloud[16:24]
+        # min_dist = np.amin(point_cloud)
+        # mem.storeState("forward", min_dist)
+
 
 class Legs_Detector():
     ''''
