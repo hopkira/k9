02d3d429fbb6f69793174e85a2ff0309477cc3de
Richard Hopkins <hopkira@googlemail.com>
2022-08-22 18:02:15 +0100

    Tweak 2

--- k9_speechserver.py
+++ k9_speechserver.py
@@ -26,44 +26,44 @@ SOX_PITCH_DEFAULT = 0
 SOX_PITCH_DOWN = -100
 
 def speak(speech:str) -> None:
-        '''
-        Break speech up into clauses using | and speak each one with
-        various pitches, volumes and distortions
-        to make the voice more John Leeson like
-        > will raise the pitch and amplitude
-        < will lower it
-        '''
-        mem.storeState("speaking",True)
-        print('Speech server:', speech)
-        speaking = None
-        clauses = speech.split("|")
-        for clause in clauses:
-            if clause and not clause.isspace():
-                if clause[:1] == ">":
-                    clause = clause[1:]
-                    pitch = PITCH_DEFAULT
-                    speed = SPEED_DOWN
-                    amplitude = AMP_UP
-                    sox_vol = SOX_VOL_UP
-                    sox_pitch = SOX_PITCH_UP
-                elif clause[:1] == "<":
-                    clause = clause[1:]
-                    pitch = PITCH_DOWN
-                    speed = SPEED_DOWN
-                    amplitude = AMP_DOWN
-                    sox_vol = SOX_VOL_DOWN
-                    sox_pitch = SOX_PITCH_DOWN
-                else:
-                    pitch = PITCH_DEFAULT
-                    speed = SPEED_DEFAULT
-                    amplitude = AMP_DEFAULT
-                    sox_vol = SOX_VOL_DEFAULT
-                    sox_pitch = SOX_PITCH_DEFAULT
-                #cmd = "espeak -v en-rp '%s' -p %s -s %s -a %s -z" % (clause, pitch, speed, amplitude)
-                cmd = ['espeak','-v','en-rp',str(clause),'-p',str(pitch),'-s',str(speed),'-a',str(amplitude)]
-                speaking = Popen(cmd)
-                Popen.wait(speaking)
-        mem.storeState("speaking",False)
+    '''
+    Break speech up into clauses using | and speak each one with
+    various pitches, volumes and distortions
+    to make the voice more John Leeson like
+    > will raise the pitch and amplitude
+    < will lower it
+    '''
+    mem.storeState("speaking",True)
+    print('Speech server:', speech)
+    speaking = None
+    clauses = speech.split("|")
+    for clause in clauses:
+        if clause and not clause.isspace():
+            if clause[:1] == ">":
+                clause = clause[1:]
+                pitch = PITCH_DEFAULT
+                speed = SPEED_DOWN
+                amplitude = AMP_UP
+                sox_vol = SOX_VOL_UP
+                sox_pitch = SOX_PITCH_UP
+            elif clause[:1] == "<":
+                clause = clause[1:]
+                pitch = PITCH_DOWN
+                speed = SPEED_DOWN
+                amplitude = AMP_DOWN
+                sox_vol = SOX_VOL_DOWN
+                sox_pitch = SOX_PITCH_DOWN
+            else:
+                pitch = PITCH_DEFAULT
+                speed = SPEED_DEFAULT
+                amplitude = AMP_DEFAULT
+                sox_vol = SOX_VOL_DEFAULT
+                sox_pitch = SOX_PITCH_DEFAULT
+            #cmd = "espeak -v en-rp '%s' -p %s -s %s -a %s -z" % (clause, pitch, speed, amplitude)
+            cmd = ['espeak','-v','en-rp',str(clause),'-p',str(pitch),'-s',str(speed),'-a',str(amplitude)]
+            speaking = Popen(cmd)
+            Popen.wait(speaking)
+    mem.storeState("speaking",False)
 
 def mqtt_callback(client, userdata, message):
     """
