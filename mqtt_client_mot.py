7b43ab7c237097c79061d115127d79466bca2dc1
Richard Hopkins <hopkira@googlemail.com>
2022-08-22 13:24:16 +0100

    Simple test program

--- mqtt_client_mot.py
+++ mqtt_client_mot.py
@@ -15,11 +15,11 @@ class K9MQTT:
     def __init__(self):
         ''' Initialise K9 in his waiting state. '''
 
-        self.last_message = ""
+        # self.last_message = ""
         self.client = mqtt.Client("k9-motor")
         self.client.connect("localhost")
         self.client.on_message = self.mqtt_callback # attach function to callback
-        self.client.subscribe("/ble/advertise/watch/m")
+        # self.client.subscribe("/ble/advertise/watch/m")
         self.client.subscribe("k9/events/motor", qos = 2)
 
     def mqtt_callback(self, client, userdata, message):
@@ -29,10 +29,10 @@ class K9MQTT:
         """
         payload = str(message.payload.decode("utf-8"))
         print(message.topic, payload)
-        if payload != self.last_message:
-            self.last_message = payload
-            event = payload[3:-1].lower()
-            print("Event: ",str(event))
+        #if payload != self.last_message:
+        #    self.last_message = payload
+        #event = payload[3:-1].lower()
+        print("Event:",str(payload))
 
 try:
     # adding comment
@@ -40,13 +40,15 @@ try:
     k9 = K9MQTT()
     k9.client.loop_start()
     print("MQTT loop started")
-    while True:
-        for msg_num in range(10):
+    #while True:
+    #    for msg_num in range(10):
             # k9.client.loop(0.1)
-            message =  "Send to Audio Event " + str(msg_num)
-            k9.client.publish("k9/events/audio", payload = message, qos = 2, retain = False)
-            print(message)
-            time.sleep(1.0)
+            #message =  "Send to Audio Event " + str(msg_num)
+            #k9.client.publish("k9/events/audio", payload = message, qos = 2, retain = False)
+            #print(message)
+    #        time.sleep(1.0)
+    while True:
+        pass
 except KeyboardInterrupt:
     k9.client.loop_stop()
     print('Exiting K9 client.')
