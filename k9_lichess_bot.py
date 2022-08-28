bd534d6db2c3b4e32556b3c0fd54d9290673c894
Richard Hopkins <hopkira@googlemail.com>
2022-08-28 14:54:31 +0100

    Simplification

--- k9_lichess_bot.py
+++ k9_lichess_bot.py
@@ -3,9 +3,9 @@ import chess.engine
 import random
 import os
 import json
+import time
 
 from lichess import Lichess
-from model import Game
 
 from k9tts import speak
 from listen import Listen
@@ -46,6 +46,51 @@ mate_lose = ("this is not possible","how can I be losing?","you are the better p
 draw = ("We are heading for a draw","The game is looking very even", "This is a well-balanced game", "We are drawing, who will make the winning move?")
 instruction = ("Please move my","I will move","I will move my","My move is")
 context = {}
+
+class Game:
+    def __init__(self, json, username, base_url, abort_time):
+        self.username = username
+        self.id = json.get("id")
+        self.speed = json.get("speed")
+        clock = json.get("clock") or {}
+        ten_years_in_ms = 1000 * 3600 * 24 * 365 * 10
+        self.clock_initial = clock.get("initial", ten_years_in_ms)
+        self.clock_increment = clock.get("increment", 0)
+        self.perf_name = (json.get("perf") or {}).get("name", "{perf?}")
+        self.initial_fen = json.get("initialFen")
+        self.state = json.get("state")
+        self.base_url = base_url
+        self.white_starts = self.initial_fen == "startpos" or self.initial_fen.split()[1] == "w"
+        self.abort_at = time.time() + abort_time
+        self.terminate_at = time.time() + (self.clock_initial + self.clock_increment) / 1000 + abort_time + 60
+        self.disconnect_at = time.time()
+
+    def is_abortable(self):
+        return len(self.state["moves"]) < 6
+
+    def ping(self, abort_in, terminate_in, disconnect_in):
+        if self.is_abortable():
+            self.abort_at = time.time() + abort_in
+        self.terminate_at = time.time() + terminate_in
+        self.disconnect_at = time.time() + disconnect_in
+
+    def should_abort_now(self):
+        return self.is_abortable() and time.time() > self.abort_at
+
+    def should_terminate_now(self):
+        return time.time() > self.terminate_at
+
+    def should_disconnect_now(self):
+        return time.time() > self.disconnect_at
+
+    def my_remaining_seconds(self):
+        return (self.state["wtime"] if self.is_white else self.state["btime"]) / 1000
+
+    def __str__(self):
+        return f"{self.url()} {self.perf_name} vs {self.opponent.__str__()}"
+
+    def __repr__(self):
+        return self.__str__()
 
 def get_phrase():
     if (context['score'].is_mate()):
