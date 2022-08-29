7a8510dc709f3afb6158b7ef12af280439cd02a0
Richard Hopkins <hopkira@googlemail.com>
2022-08-29 17:36:30 +0100

    Quick loop

--- test.py
+++ test.py
@@ -82,3 +82,6 @@ print("Initial state:",str(initial_state))
 game = Game(initial_state, username, li.baseUrl, 20)
 moves = game.state["moves"].split()
 print("Moves:",moves)
+while True:
+    for event in stream:
+        print("Event:",str(event))
