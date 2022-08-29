e3ec5df377d0ed21e8d3479f273649f4cdb265c5
Richard Hopkins <hopkira@googlemail.com>
2022-08-29 16:07:38 +0100

    Test 4

--- test.py
+++ test.py
@@ -13,18 +13,15 @@ li = Lichess(token=bot_token, url=lichess_url)
 color = "white"
 side = "white"
 
-# player_token = {"Authorization": "Bearer {}".format(player_token)}
+player_token = str({"Authorization": "Bearer {}".format(player_token)})
 
 def create_game(username, token, color):
     params = {"rated": False, 
                 "variant": "standard",
-                "clock": {
-                    "limit":300,
-                    "increment":15
-                },
+                "clock.limit": 300.0,
+                "clock.increment": 15,
                 "color": color,
-                "acceptByToken": token,
-                "message": "Affirmative. Your game with K9 is ready: {game}.!"
+                "acceptByToken": token
                 }
     print(username, params)
     response = li.create_challenge(username, params)
