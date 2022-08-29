61d2f72c69287e759b26b0c86d0969fb918782a2
Richard Hopkins <hopkira@googlemail.com>
2022-08-29 15:36:37 +0100

    Test

--- test.py
+++ test.py
@@ -10,16 +10,21 @@ lichess_url = "https://lichess.org/api/"
 
 li = Lichess(token=bot_token, url=lichess_url)
 
+color = "white"
 side = "white"
 
-def create_game(username, token:str, color:str):
+player_token = {"Authorization": "Bearer {}".format(player_token)}
+
+def create_game(username, token, color):
     params = {"rated": False, 
                 "variant": "standard",
-                "clock.limit": 300,
-                "clock.increment": 15,
+                "clock": {
+                    "limit":300,
+                    "increment":15
+                },
                 "color": color,
                 "acceptByToken": token,
-                "message": "K9 is ready to play!"
+                "message": "Affirmative. Your game with K9 is ready: {game}.!"
                 }
     print(username, params)
     response = li.create_challenge(username, params)
