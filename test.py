cec58b6a8438b4d81d68d3cf6467453f63ddb97f
Richard Hopkins <hopkira@googlemail.com>
2022-08-30 18:22:15 +0100

    API

--- test.py
+++ test.py
@@ -59,7 +59,7 @@ stockfish_path = os.getenv("STOCKFISH_PATH")
 username = os.getenv("LICHESS_USERNAME")
 lichess_url = "https://lichess.org/api/"
 
-li = Lichess(token=bot_token, url=lichess_url)
+li = LichessAPI(token=bot_token, url=lichess_url)
 
 engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
 INFO_SCORE = 2
