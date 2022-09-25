d6842f00a7efe92feb44481097a41779ea9764f6
Richard Hopkins <hopkira@googlemail.com>
2022-09-25 11:47:13 +0100

    Absoloute paths

--- k9.sh
+++ k9.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/bin/python3 ./k9_streaming_tts.py &
+python3 /home/pi/k9/k9_streaming_tts.py &
 ./start.sh depthAI k9_oak_pipe.py &
 ./start.sh depthAI lidar360.py &
-/usr/bin/python3 ./k9_audio_sm.py &
+python3 /home/pi/k9/k9_audio_sm.py &
