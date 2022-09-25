1d6345872c79a177208f07426f6d3c905c4736da
Richard Hopkins <hopkira@googlemail.com>
2022-09-25 11:26:15 +0100

    audo

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-# if virtualenvwrapper.sh is in your PATH (i.e. installed with pip)
-source 'which virtualenvwrapper.sh'
-#source /path/to/virtualenvwrapper.sh # if it's not in your PATH
+source /usr/local/bin/virtualenvwrapper.sh
 workon $1
 python $2
 deactivate
