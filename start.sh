64ee79083fc2ee757889a4cc1889b5c05687d50c
Richard Hopkins <hopkira@googlemail.com>
2022-09-25 11:23:38 +0100

    auto

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# start environment for python program
-source `which virtualenvwrapper.sh`
+# if virtualenvwrapper.sh is in your PATH (i.e. installed with pip)
+source 'which virtualenvwrapper.sh'
+#source /path/to/virtualenvwrapper.sh # if it's not in your PATH
 workon $1
 python $2
 deactivate
