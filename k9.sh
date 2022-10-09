a16af4b45bf902320030371085606a77264f18dc
Richard Hopkins <hopkira@googlemail.com>
2022-10-09 16:19:40 +0100

    Activate motor controller

--- k9.sh
+++ k9.sh
@@ -3,4 +3,4 @@ python3 /home/pi/k9/k9_streaming_tts.py & # speech server
 ./start.sh depthAI k9_oak_pipe.py & # 3D camera pipeline
 ./start.sh depthAI k9_lidar360.py & # Rear LIDAR collision detect
 python3 /home/pi/k9/k9_audio_sm.py & # Conversation state machine
-#python3 /home/pi/k9/k9_motor_sm.py & # Motor controller state machine
+python3 /home/pi/k9/k9_motor_sm.py & # Motor controller state machine
