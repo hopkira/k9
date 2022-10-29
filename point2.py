984661ccd5004e849ef56329b5e8b16279c8c755
Richard Hopkins <hopkira@googlemail.com>
2022-10-29 16:27:31 +0100

    Simplify

--- point2.py
+++ point2.py
@@ -24,26 +24,22 @@ xoutDisparity = pipeline.create(dai.node.XLinkOut)
 xoutDepth = pipeline.create(dai.node.XLinkOut)
 xoutRectifLeft = pipeline.create(dai.node.XLinkOut)
 xoutRectifRight = pipeline.create(dai.node.XLinkOut)
-
 camLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
 camRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
-
-for monoCam in (camLeft, camRight):  # Common config
-    monoCam.setResolution(dai.MonoCameraProperties.SensorResolution.THE_480_P)
-
+camLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_480_P)
+camRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_480_P)
 stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_ACCURACY)
-stereo.initialConfig.setMedianFilter(dai.StereoDepthProperties.MedianFilter.KERNEL_7x7)  # KERNEL_7x7 default
-stereo.setRectifyEdgeFillColor(0)  # Black, to better see the cutout
+stereo.initialConfig.setMedianFilter(dai.StereoDepthProperties.MedianFilter.KERNEL_7x7)
+stereo.setRectifyEdgeFillColor(0)
 stereo.setLeftRightCheck(True)
 stereo.setExtendedDisparity(False)
 stereo.setSubpixel(True)
-
-xoutLeft.setStreamName("left")
-xoutRight.setStreamName("right")
-xoutDisparity.setStreamName("disparity")
+#xoutLeft.setStreamName("left")
+#xoutRight.setStreamName("right")
+#xoutDisparity.setStreamName("disparity")
 xoutDepth.setStreamName("depth")
-xoutRectifLeft.setStreamName("rectifiedLeft")
-xoutRectifRight.setStreamName("rectifiedRight")
+#xoutRectifLeft.setStreamName("rectifiedLeft")
+#xoutRectifRight.setStreamName("rectifiedRight")
 
 camLeft.out.link(stereo.left)
 camRight.out.link(stereo.right)
@@ -58,11 +54,11 @@ with device:
     print("Starting pipeline")
     device.startPipeline(pipeline)
     print("Pipeline started")
-    qDepth = device.getOutputQueue(name = "depth", maxSize = 1, blocking = False)
+    qDepth = device.getOutputQueue(name = "depth", maxSize = 8, blocking = False)
     print("Ready to process images")
     while True:
         frame = qDepth.get().getCvFrame()
-        frame = getDepthFrame(frame)
-        cv2.imshow("False Depth Image", frame)
+        im_frame = getDepthFrame(frame)
+        cv2.imshow("False Depth Image", im_frame)
         if cv2.waitKey(1) == ord("q"):
             break
