9724f06b4ff51f0206962e703834244baa871dcc
Richard Hopkins <hopkira@googlemail.com>
2022-10-29 18:54:26 +0100

    Deltas

--- point2.py
+++ point2.py
@@ -21,19 +21,15 @@ class Fwd_Collision_Detect():
         # Point cloud loop constants - ranges      
         self.x_bins = pd.interval_range(start = -350, end = 350, periods = 7)
         self.y_bins = pd.interval_range(start = 0, end = 800, periods = 8)
-        print("xbin:",self.x_bins)
-        print("ybin:",self.y_bins)
         self.fx = 1.4 # values found by measuring known sized objects at known distances
-        self.fy = 2.2 # originally 2.05
+        self.fy = 2.05 # originally 2.05
         self.pc_width = 640
         self.cx = self.pc_width / 2
         self.pc_height = 480
         self.cy = self.pc_height / 2
-        self.pc_max_range  = 10000.0
+        self.pc_max_range  = 5000.0
         self.pc_min_range  = 200.0
         self.column, self.row = np.meshgrid(np.arange(self.pc_width), np.arange(self.pc_height), sparse=True)
-        print("cols:",self.column)
-        print("rows:",self.row)
 
     def record_min_dist(self,depth_image) -> float:
         '''
@@ -46,7 +42,7 @@ class Fwd_Collision_Detect():
         # Calculate the point cloud using simple extrapolation from depth
         z = np.where(valid, depth_image, 0.0)
         x = np.where(valid, (z * (self.column - self.cx) /self.cx / self.fx) + 120.0 , self.pc_max_range)
-        y = np.where(valid, 270.0 - (z * (self.row - self.cy) / self.cy / self.fy) , self.pc_max_range) # measured height is 268mm
+        y = np.where(valid, 268.0 - (z * (self.row - self.cy) / self.cy / self.fy) , self.pc_max_range) # measured height is 268mm
         z2 = z.flatten()
         x2 = x.flatten()
         y2 = y.flatten()
