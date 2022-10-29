a28310eadff2c426678dc1a48d3dda953d857915
Richard Hopkins <hopkira@googlemail.com>
2022-10-29 15:32:29 +0100

    Remove list

--- point2.py
+++ point2.py
@@ -58,7 +58,7 @@ with device:
     print("Starting pipeline")
     device.startPipeline(pipeline)
     print("Pipeline started")
-    qDepth = [device.getOutputQueue(name = "depth", maxSize = 1, blocking = False)]
+    qDepth = device.getOutputQueue(name = "depth", maxSize = 1, blocking = False)
     print("Ready to process images")
     while True:
         inDepth = qDepth.get().getCvFrame()
