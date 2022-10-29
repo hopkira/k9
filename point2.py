748558eded3ea47e2489d6f7b9a884f297a98e1a
Richard Hopkins <hopkira@googlemail.com>
2022-10-29 15:08:55 +0100

    Change to hot

--- point2.py
+++ point2.py
@@ -16,8 +16,8 @@ def getDepthFrame(frame):
     max = np.amax(frame)
     print(min,max)
     # disp = (frame * (65535.0 / max)).astype(np.uint16)
-    disp = (frame / 33998.0 * 255).astype(np.uint8)
-    disp = cv2.applyColorMap(disp, cv2.COLORMAP_JET)
+    disp = (frame / 255.0).astype(np.uint8)
+    disp = cv2.applyColorMap(disp, cv2.COLORMAP_HOT)
     return disp
 
 print("Creating Stereo Depth pipeline")
