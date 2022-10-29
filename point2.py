9d545008c954d46bf1c2fc2c64ddf6b7a916114b
Richard Hopkins <hopkira@googlemail.com>
2022-10-29 18:46:20 +0100

    285

--- point2.py
+++ point2.py
@@ -46,7 +46,7 @@ class Fwd_Collision_Detect():
         # Calculate the point cloud using simple extrapolation from depth
         z = np.where(valid, depth_image, 0.0)
         x = np.where(valid, (z * (self.column - self.cx) /self.cx / self.fx) + 120.0 , self.pc_max_range)
-        y = np.where(valid, 275.0 - (z * (self.row - self.cy) / self.cy / self.fy) , self.pc_max_range) # measured height is 268mm
+        y = np.where(valid, 285.0 - (z * (self.row - self.cy) / self.cy / self.fy) , self.pc_max_range) # measured height is 268mm
         z2 = z.flatten()
         x2 = x.flatten()
         y2 = y.flatten()
