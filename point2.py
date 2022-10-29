a961196335e4a241b5f7759433e27ba035fd67a4
Richard Hopkins <hopkira@googlemail.com>
2022-10-29 15:05:18 +0100

    High accuracy

--- point2.py
+++ point2.py
@@ -39,7 +39,7 @@ camRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 for monoCam in (camLeft, camRight):  # Common config
     monoCam.setResolution(dai.MonoCameraProperties.SensorResolution.THE_480_P)
 
-stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_QUALITY)
+stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_ACCURACY)
 stereo.initialConfig.setMedianFilter(dai.StereoDepthProperties.MedianFilter.KERNEL_7x7)  # KERNEL_7x7 default
 stereo.setRectifyEdgeFillColor(0)  # Black, to better see the cutout
 stereo.setLeftRightCheck(True)
