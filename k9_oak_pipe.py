af36e2d6e41d356ff51468efa76cbd5dee7d89c9
Richard Hopkins <hopkira@googlemail.com>
2022-10-30 17:38:52 +0000

    Fix scale

--- k9_oak_pipe.py
+++ k9_oak_pipe.py
@@ -615,7 +615,7 @@ with dai.Device(pipeline) as device:
             in_rgb = qRgb.get()
             preview = in_rgb.getCvFrame() # get RGB frame
             # Resize frame to fit Pi VNC viewer
-            scale = 1
+            scale = 0.25
             width = int(preview.shape[1] * scale)
             height = int(preview.shape[0] * scale)
             dsize = (width, height)
