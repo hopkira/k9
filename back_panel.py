891f3bbe03286ff96d2169b22f09056c88bc34ea
Richard Hopkins <hopkira@googlemail.com>
2022-11-04 18:58:31 +0000

    Tweaks

--- back_panel.py
+++ back_panel.py
@@ -26,54 +26,15 @@ class BackLights():
             timeout=10
             )
 
-    def command(self,text:str) -> None:
-        print("Back panel:",text)
-        self.ser.write(str.encode(text+"\n"))
-
     def __write(self,text:str) -> None:
         print("Back panel:",text)
         self.ser.write(str.encode(text+"\n"))
 
+    def cmd(self,text:str) -> None:
+        self.__write(text)
+
     def on(self):
-        self.level = 1
         self.__write("on")
 
     def off(self):
-        self.level = 0
         self.__write("off")
-
-'''
-import pyboard
-
-class BackLights():
-    def __init__(self) -> None:
-        self.level = 0
-        self.pyb = pyboard.Pyboard('/dev/tty.usbmodem388D384731342')
-
-    def __write(self, text:str) -> None:
-        print("Back panel:",text)
-        self.pyb.enter_raw_repl()
-        print(self.pyb.exec(text))
-        self.pyb.exit_raw_repl()
-
-    def ledoff(self) -> None:
-        self.__write("on")
-
-    def flash(self) -> None:
-        self.__write("off")
-
-    def on(self):
-        self.level = 1
-        self.state()
-
-    def off(self):
-        self.level = 0
-        self.state()
-
-    def state(self):
-        value = int(self.level * 65535)
-        #pca.channels[1].duty_cycle = value
-
-    def get_level(self):
-        return self.level
-'''
