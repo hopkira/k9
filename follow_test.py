2107a8e02b4c51e61e762e26c8c1cb2c010f220f
Richard Hopkins <hopkira@googlemail.com>
2022-11-20 16:05:40 +0000

    Refined movement

--- follow_test.py
+++ follow_test.py
@@ -1,39 +1,47 @@
 from memory import Memory
 import time
 
+def get_target(dict:dict) -> tuple:
+    try:
+        if dict["angle"] != 0 or dict["distance"] != 0 :
+            print(str(dict))
+            angle = dict["angle"]
+            move = dict["distance"]
+            return angle, move
+    except KeyError:
+        return False, False
+    
+def check_targets() -> tuple:
+    angle = False
+    move = False
+    source = ""
+    target_dict = mem.retrieveLastSensorReading("follow")
+    person_dict = mem.retrieveLastSensorReading("person")
+    if any(get_target(target_dict)):
+        angle, move = get_target(target_dict)
+        print("Legs:", angle, move)
+    elif any(get_target(person_dict)):
+        angle, move = get_target(person_dict)
+        print("Person:", angle, move)
+    else:
+        target_dicts = mem.retrieveSensorReadings("follow")
+        for target_dict in target_dicts:
+            if any(get_target(target_dict)):
+                angle, move = get_target(target_dict)
+                age = target_dict["time"] - time.time()
+                print(age,"s - old legs:", angle, move)
+                break
+    return angle, move
+
 mem = Memory()
 while True:
     time.sleep(1)
     angle = 0
     move = 0
-    target_dict = mem.retrieveLastSensorReading("follow")
-    person_dict = mem.retrieveLastSensorReading("person")
-    # chose the detected legs over person targetting
-    if target_dict["angle"] != 0 or target_dict["distance"] != 0 :
-        print("Active legs being used")
-        angle = target_dict["angle"]
-        move = target_dict["distance"]
-    elif person_dict["angle"] !=0 or person_dict["distance"] != 0:
-        print("Active person being used")
-        angle = person_dict["angle"]
-        move = person_dict["distance"]
-    # if there is nothing detected, then aim for
-    # the last detected set of legs
-    else:
-        target_dicts = mem.retrieveSensorReadings("follow")
-        for target_dict in target_dicts:
-            try:
-                angle = target_dict["angle"]
-                age = target_dict["time"] - time.time()
-                if angle != 0:
-                    print("Historic angle of ", str(angle),"radians and",round(age),"seconds old" )
-                    break
-            except KeyError:
-                print("No historic legs found")
-                break
+    angle, move = check_targets()
     # move if the angle or distance is not zero
     if angle != 0 or move !=0:
-        print("Following: direction:", angle, "distance:", move)
+        print("Target dir:", angle, "dist:", move)
         damp_angle = 3.0
         damp_distance = 2.0
         if abs(angle) >= (0.1 * damp_angle) :
@@ -46,10 +54,18 @@ while True:
             if abs(move) >= (0.05 * damp_distance) :
                 distance = move / damp_distance
                 safe_forward = mem.retrieveState("forward")
+                safe_backward = mem.retrieveState("reverse")
                 # nb should also retrieve a backward state
-                if  safe_forward > distance:
-                    print("Moving forward detected distance: ", str(distance) )
+                if distance >= 0:
+                    if  safe_forward > distance:
+                        print("Moving forward detected distance:", str(distance))
+                    else:
+                        print("Moving forward safe distance:", str(safe_forward))
                 else:
-                    print("Moving forward safe distance: ", str(safe_forward))
+                    if safe_backward < distance:
+                        print("Moving backward detected distance:", str(distance))
+                    else:
+                        print("Moving backward safe distance:", str(safe_backward))
+
     else:
         print("Nothing detected, not moving")
