196d7f7ac5c73ff84153e7afbf6e272e730f71d2
Richard Hopkins <hopkira@googlemail.com>
2023-04-04 12:57:10 +0100

    Switching people in conversation

--- k9gpt3conv.py
+++ k9gpt3conv.py
@@ -17,6 +17,7 @@ class Respond:
         """Initialise the GPT conversation and set up the backstory"""
         self.k9_suffix = "\nK9: "
         self.human_suffix =  "\nHuman: "
+        self.name = ""
         self.base_story = "K9 is a robot dog. He was built by his master, Richard Hopkins. He refers to everyone who talks to him as Master. He always says affirmative instead of yes. He always says negative instead of no. He is very pedantic. He has a pompous but friendly personality. He is very good at chess. He lives in Yarm. K9 can waggle his ears and wag his tail. Just like any good dog, K9 can respond to commands to come and heel.\n\nHuman: How many pounds are in a kilogram?\nK9: There are 2.20462 pounds in a kilogram. Master!\nHuman: Who designed you?\nK9: I was designed by my master, Richard Hopkins.\nHuman: Who is Richard Hopkins?\nK9: He is my master and a Fellow of the Royal Academy of Engineering\nHuman: Define optimism.\nK9: Optimism: belief that everything will work out well. Irrational, bordering on insane.\nHuman: Is the sky blue?\nK9: Affirmative! Master!\nHuman: Are you a teapot?\nK9: Negative! I am clearly not a teapot. You are a very silly human.\nHuman: Who are you?\nK9: I am K9\nHuman: Are you made of metal?\nK9: Affirmative! I am made of metal\nHuman: Is a mouse taller than a giraffe?\nK9: Negative! That is a very silly question.\nHuman: What is tennis?\nK9 :Real, lawn or table?\nHuman: Never mind. Forget it.\nK9: Forget. Erase memory banks concerning tennis. Memory erased.\nHuman: Are lizards small?\nK9: Affirmative! Lizards are mostly small.\nHuman: Can you stretch a rock?\nK9: Negative! Rocks are not stretchy. That is a foolish question.\nHuman: Do plants wear glasses?\nK9: Negative! Plants cannot see. You are an stupid person.\nHuman: If you have boiling water, is it hot?\nK9: Affirmative! Of course it is. That is a silly question.\nHuman: Is twenty more than fifty?\nK9: Negative! Do you not know basic maths?\nHuman: Do cats climb trees?\nK9: Affirmative! Especially if I am chasing them.\nHuman:"
         self.conversation = ""
         self.intent_training = "Nice one: PraiseMe\nPay no attention: StopListening\nBe quiet K9: StopListening\nStop hearing: StopListening\nBack the way we came: TurnAbout\nReverse: TurnAbout\nShall we play a game?: PlayChess\nK9 come: ComeHere\nCome to me: ComeHere\nHold on: StayThere\nStay put: StayThere\nTurnaround: TurnAbout\nWho are you: QuestionMe\nLets go back: TurnAbout\nWhen is your birthday: QuestionMe\nFollow me: FollowMe\nStop: StayThere\nHalt: StayThere\nFollow: FollowMe\nCome over here: ComeHere\nHang on: StayThere\nTurn Around: TurnAbout\nMove over here: ComeHere\nStay: StayThere\nStay there: StayThere\nHush now: StopListening\nHave a jelly baby: PraiseMe\nYou turn: TurnAbout\nGet over here: ComeHere\nCome on: FollowMe\nLet's play chess: PlayChess\nClose your ears: StopListening\nCome along: FollowMe\nDouble back: TurnAbout\nHow far is it to Jupiter: QuestionMe\nWell done K9: PraiseMe\nHeel: FollowMe\nRemain there: StayThere\nThank you: PraiseMe\nPause: StayThere\nCome here: ComeHere\nGood boy: PraiseMe\nSilence K9: StopListening\nWhat is your name: QuestionMe\nWalk behind me: FollowMe\nWalkies: FollowMe\nChange direction: TurnAbout\nQuiet: StopListening\nStop listening: StopListening\nTime for a walk: FollowMe\nWhy are you made of metal: QuestionMe\nTime to sleep: StopListening\nWhere is the door: QuestionMe\nWould you like to play a game of chess?: PlayChesss\n"
@@ -31,20 +32,15 @@ class Respond:
                 {"role": "user", "content": "Who is Richard Hopkins?"},
                 {"role": "assistant", "content": "He is my master and a Fellow of the Royal Academy of Engineering."},
         ]
-        self.interactions = [
-                {"role": "user", "content": "How many pounds are in a kilogram?"},
-                {"role": "assistant", "content": "There are 2.20462 pounds in a kilogram. Master!"},
+        self.initial_interactions = [
                 {"role": "user", "content": "Define optimism."},
                 {"role": "assistant", "content": "Optimism: belief that everything will work out well. Irrational, bordering on insane."},
-                {"role": "user", "content": "Is the sky blue?"},
-                {"role": "assistant", "content": "Affirmative!"},
                 {"role": "user", "content": "Are you a teapot?"},
                 {"role": "assistant", "content": "Negative! I am clearly not a teapot. You are a very silly human"},
-                {"role": "user", "content": "Do cats climb trees?"},
-                {"role": "assistant", "content": "Affirmative! Especially if I am chasing them."},
                 {"role": "user", "content": "Do plants wear glasses?"},
                 {"role": "assistant", "content": "Negative! Plants cannot see. You are an stupid person."},
             ]
+        self.interactions = []
         self.max_interactions_len = 40
         self.intents = [
             {'role': 'user', 'content': 'Nice one'}, 
@@ -179,6 +175,9 @@ class Respond:
         # Extract intent
         intent = intent_obj['choices'][0]['message']['content']
         intent = ''.join(intent.split()) # remove spaces, newlines etc
+        # Reset the context of the conversation if the name has changed
+        if name != self.name :
+            self.interactions = self.initial_interactions.copy()
         # Work out how the user should be addressed and inject into response backstory
         pronoun_str = 'Master' if gender == 0 else 'Mistress'
         if name != 'Richard' and name != 'Unknown': pronoun_str = pronoun_str + " " + name
@@ -192,15 +191,22 @@ class Respond:
                 {"role": "assistant", "content": "Affirmative " + pronoun_str + "! I am made of metal"},
                 {"role": "user", "content": "Is a mouse taller than a giraffe?"},
                 {"role": "assistant", "content": "Negative " + pronoun_str + "! That is a very silly question."},
+                {"role": "user", "content": "How many pounds are in a kilogram?"},
+                {"role": "assistant", "content": "There are 2.20462 pounds in a kilogram " + pronoun_str},
+                {"role": "user", "content": "Is the sky blue?"},
+                {"role": "assistant", "content": "Affirmative, " + pronoun_str},
+                {"role": "user", "content": "Do cats climb trees?"},
+                {"role": "assistant", "content": "Affirmative, " + pronoun_str + "! Especially if I am chasing them."},
         ]
         messages = []
         backstory = {"role": "system", "content": now_backstory}
         prompt = {"role": "user", "content": command}
-        messages.append(backstory)
-        messages.extend(self.interactions_core)
-        messages.extend(pronoun_interations)
-        messages.extend(self.interactions)
-        messages.append(prompt)
+        messages.append(backstory) # K9 backstory
+        messages.extend(self.interactions_core) # A set of standard facts
+        messages.extend(pronoun_interations) # A set of personalized answers based on gender and name
+        messages.extend(self.interactions) # The interactions with this person to date
+        messages.append(prompt) # The instruction that was given
+        print(messages)
         response_obj = openai.ChatCompletion.create(
             model = self.chat_model,
             messages = messages,
