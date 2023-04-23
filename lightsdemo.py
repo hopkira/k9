fcb8c4fee2942d04f925604f82341136e78bab36
Richard Hopkins <hopkira@googlemail.com>
2023-04-23 17:52:12 +0100

    Update lightsdemo.py

--- lightsdemo.py
+++ lightsdemo.py
@@ -4,7 +4,7 @@ from  voice import Voice
 bl = BackLights()
 k9voice =  Voice()
 
-modes = ["original","colour","colourflow","diagnonal","two","three","four","six","red","green","blue","rows","on"]
+modes = ["original","colour","colourflow","diagnonal","two","three","four","six","red","green","blue","rows","on","off"]
 for mode in modes:
     bl.cmd(mode)
     k9voice.speak(mode)
