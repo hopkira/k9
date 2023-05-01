1d2c12e8e89a6d107d47f50416bdded523622907
Richard Hopkins <hopkira@googlemail.com>
2023-05-01 15:42:05 +0100

    Update get_key.py

--- get_key.py
+++ get_key.py
@@ -5,9 +5,7 @@ bp.cmd("computer")
 original = bp.get_switch_state()
 print("original:",str(original))
 while True:
-        time.sleep(0.5)
         new = bp.get_switch_state()
-        print("new     :",str(new))
         for i,state in enumerate(new):
                 if state != original[i]:
                         print("Button",i,"pressed.")
