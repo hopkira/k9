162536ae17d3f1cedc81843e957cd7ef9fafd7dc
Richard Hopkins <hopkira@googlemail.com>
2023-05-01 15:23:18 +0100

    Update get_key.py

--- get_key.py
+++ get_key.py
@@ -1,8 +1,10 @@
 from back_panel import BackLights
 bp = BackLights()
 original = bp.get_switch_state()
+print("original:",str(original))
 while True:
         new = bp.get_switch_state()
+        print("new     :",str(new))
         for i,state in enumerate(new):
                 if state != original[i]:
                         print("Button",i,"pressed.")
