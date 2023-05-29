271a156fcc7a287517c39cd447a740e3fb4d966f
Richard Hopkins <hopkira@googlemail.com>
2023-05-29 14:57:43 +0100

    Update test_switches.py

--- test_switches.py
+++ test_switches.py
@@ -2,6 +2,7 @@ from back_panel import BackLights
 import time
 k9lights = BackLights()
 turn_on_lights = [1,2,3,5,12]
+k9lights.turn_on(turn_on_lights)
 while True:
     state = k9lights.get_switch_state()
     print(state)
