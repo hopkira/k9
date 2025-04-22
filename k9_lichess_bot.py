9467f37b76a44fc133dc6a1dc0b586abb10ad378
Richard Hopkins <hopkira@googlemail.com>
2025-04-22 15:27:25 +0100

    Update k9_lichess_bot.py

--- k9_lichess_bot.py
+++ k9_lichess_bot.py
@@ -255,13 +255,13 @@ class ChessGame():
                                     move = result.move
                                     print("Out of book moves!")
                                     print("Stockfish Move: {}".format(move))
-                                    score = result.score.pov(chess.WHITE)
+                                    score = result.info['score'].pov(chess.WHITE)
                                 else:
                                     result = self.engine.analyse(board=self.board, limit=chess.engine.Limit(time=1.0),info=INFO_SCORE)
                                     print("Result: {}".format(result))
                                     move = book_move.move
                                     print("Book Move: {}".format(move))
-                                    score = result.info["score"].pov(chess.WHITE)
+                                    score = result["score"].pov(chess.WHITE)
                                 print("Final Move: {}".format(move))
                                 print("White's score: {}".format(score))
                                 self.back.off()
